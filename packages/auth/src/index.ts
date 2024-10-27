54a83df8baad1bbee4c5773fd11dcbcce55ddd78
devGabyAlves <gabriellyalves401@gmail.com>
2024-10-27 12:39:55 -0300

    feat: cargo permits

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -6,6 +6,9 @@ import {
     MongoAbility,
   } from '@casl/ability'
 
+import { User } from './models/user'
+import { permissions } from './permissions'
+
   const actions = ['manage', 'invite', 'delete'] as const
   const subjects = ['User', 'all'] as const
 
@@ -20,9 +23,15 @@ import {
   export type AppAbility = MongoAbility<AppAbilities>
   export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-  const { build, can, cannot } = new AbilityBuilder(createAppAbility)
-
-  can('invite', 'User')
-  cannot('delete', 'User')
+  export function defineAbilityFor(user: User) {
+    const builder = new AbilityBuilder(createAppAbility)
+    
+    if (typeof permissions[user.role] !== 'function') {
+      throw new Error(`Permissions for role ${user.role} not found.`)
+    }
+    permissions[user.role](user, builder)
   
-  export const ability = build()
+    const ability = builder.build()
+  
+    return ability
+  }
