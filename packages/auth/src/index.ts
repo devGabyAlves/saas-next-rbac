d5adb1c166e497632e629874b5afdf62b1df0211
devGabyAlves <gabriellyalves401@gmail.com>
2024-10-27 12:43:59 -0300

    feat: creating subject typing

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -2,30 +2,23 @@ import {
     AbilityBuilder,
     CreateAbility,
     createMongoAbility,
-    ForcedSubject,
     MongoAbility,
   } from '@casl/ability'
 
 import { User } from './models/user'
 import { permissions } from './permissions'
 
-  const actions = ['manage', 'invite', 'delete'] as const
-  const subjects = ['User', 'all'] as const
+import { ProjectSubject } from './subjects/project'
+import { UserSubject } from './subjects/user'
 
-  type AppAbilities = [
-    (typeof actions)[number],
-    (
-      | (typeof subjects)[number]
-      | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-    ),
-  ]
+type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
 
   export type AppAbility = MongoAbility<AppAbilities>
   export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
   export function defineAbilityFor(user: User) {
     const builder = new AbilityBuilder(createAppAbility)
-    
+
     if (typeof permissions[user.role] !== 'function') {
       throw new Error(`Permissions for role ${user.role} not found.`)
     }
