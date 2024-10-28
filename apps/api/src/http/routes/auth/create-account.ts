dd8b4a1d510333ac23e9928f6f27704aa4d0ea0e
devGabyAlves <gabriellyalves401@gmail.com>
2024-10-28 15:45:39 -0300

    feat: automatic association via email

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -31,14 +31,33 @@ export async function createAccount(app: FastifyInstance) {
           .status(400)
           .send({ message: 'User with same e-mail already exists.' })
       }
+
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
       const passwordHash = await hash(password, 6)
+      
       await prisma.user.create({
         data: {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       })
+
       return reply.status(201).send()
     },
   )
